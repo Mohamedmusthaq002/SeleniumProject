380dd3bff35cb541c4215e48e150bdb16290a866
Mohamedmusthaq002 <mohamedmusthaq002@gmail.com>
2024-03-09 12:56:29 +0530

    rahul added his code

--- src/com/sample/ClientDetails.java
+++ src/com/sample/ClientDetails.java
@@ -6,12 +6,9 @@ public class ClientDetails {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Rahul Added his Code");
-		System.out.println("SAMPLE");
-<<<<<<< HEAD
 		
-=======
+		System.out.println("SAMPLE");
 		System.out.println("Anu Completed his Work");
->>>>>>> d4e33951474d350546ba5596559a2b3113e10ba5
+		System.out.println("Rahul Added his Code");
 	}
 }	
